5b784ffb56420074784249753eed92250fc50548
lifeisgood1990 <940754104@qq.com>
2019-12-31 11:18:12 +0800

    modify

--- src/main/java/com/yf/producer/task/NBAScoreTask.java
+++ src/main/java/com/yf/producer/task/NBAScoreTask.java
@@ -6,6 +6,7 @@ import com.google.gson.JsonArray;
 import com.yf.producer.util.HttpClientUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -80,7 +81,7 @@ public class NBAScoreTask {
 
     @Scheduled(initialDelay = 1000 * 3, fixedDelay=Long.MAX_VALUE)
     public static void watchMatch() throws Exception{
-        int initMaxSid = 0;
+        int lastMaxSid = 0;
         Map<String,JSONObject> matchMap = getLiveList();
         if (matchMap.size() == 0){
             log.info("暂时没有比赛");
@@ -96,14 +97,11 @@ public class NBAScoreTask {
         JSONObject object = matchMap.get(matchId);
         String hostTeam = object.getString("home_team");
         String visitTeam = object.getString("visit_team");
-//        GetMaxSidThread getMaxSidThread = new GetMaxSidThread(matchId);
-//        Thread thread = new Thread(getMaxSidThread);
-//        thread.start();
+        String today =  DateFormatUtils.format(new Date(),"yyyy-MM-dd");
         String maxIdUrl = "http://dingshi4pc.qiumibao.com/livetext/data/cache/max_sid/"+matchId+"/0.htm";
         while (true){
             maxSId = Integer.parseInt(HttpClientUtil.sendGet(maxIdUrl));
-
-            if (initMaxSid == maxSId){
+            if (lastMaxSid == maxSId){
                 continue;
             }
             // 比赛内容
@@ -114,25 +112,25 @@ public class NBAScoreTask {
             }
             JSONArray jsonArray = JSONArray.parseArray(contentResult);
             for (int i = 0; i < jsonArray.size(); i++) {
+                String scoreUrl = "http://bifen4pc2.qiumibao.com/json/"+ today +"/"+matchId+".htm";
+                String scoreResult = HttpClientUtil.sendGet(scoreUrl);
+                String periodCn = "";
+                if (StringUtils.isNotBlank(scoreResult)){
+                    JSONObject detail = JSONObject.parseObject(scoreResult);
+                    periodCn = detail.getString("period_cn").replace("\n"," ");
+                }
                 JSONObject jsonObject = (JSONObject) jsonArray.get(i);
-                String  liveSid = jsonObject.getString("live_sid");
-                initMaxSid = Integer.parseInt(liveSid);
+                String liveSid = jsonObject.getString("live_sid");
+                lastMaxSid = Integer.parseInt(liveSid);
                 String homeScore = jsonObject.getString("home_score");
                 String visitScore = jsonObject.getString("visit_score");
                 String liveText = jsonObject.getString("live_text");
                 String pidText = jsonObject.getString("pid_text").replace("\n"," ");
-                log.info("sid{} {}【{}:{}】{} {} {}",liveSid,hostTeam,homeScore,visitScore,visitTeam,liveText,pidText);
-                Thread.sleep(500);
+                log.info("sid{} {}【{}:{}】{} {} {}",liveSid,hostTeam,homeScore,visitScore,visitTeam,liveText,periodCn);
+                Thread.sleep(1000);
             }
             Thread.sleep(1000);
         }
-    }
-
-    public static void main(String[] args) throws Exception {
-        watchMatch();
-        GetMaxSidThread getMaxSidThread = new GetMaxSidThread(matchId);
-        Thread thread = new Thread(getMaxSidThread);
-        thread.start();
     }
 
 }
